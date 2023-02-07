8f84c6899eda9e111d36dfbabed7d1cd49eaa007
AntonZinchenko32 <zinchenko309@gmail.com>
2023-02-07 18:54:10 +0200

    Скрипт из конспекта
    Копирует и частично рефакторит

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,19 @@
 
+
+const myForm = document.querySelector(".feedback-form");
+
+myForm.addEventListener("input", handleInput);
+
+function handleInput(event) {
+  const {
+    elements: { email, message }
+  } = event.currentTarget;
+
+  if (email.value === "" || message.value === "") {
+    return console.log("Please fill in all the fields!");
+  }
+
+  console.log(`Login: ${email.value}, Password: ${message.value}`);
+  
+  event.currentTarget.reset();
+}
