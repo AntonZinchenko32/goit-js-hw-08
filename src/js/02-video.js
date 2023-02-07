b96d98a4196e7642d4c40fee9c34a1830190458b
AntonZinchenko32 <zinchenko309@gmail.com>
2023-02-07 14:33:02 +0200

    скрипт_задания_2, библиотека lodash

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,8 +1,14 @@
 import Player from '@vimeo/player';
 
-const iframe = document.querySelector('iframe');
-const player = Player;
 
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+player.setCurrentTime(localStorage.getItem("videoplayer-current-time"));
+
+player.on('timeupdate', (data) => 
+    localStorage.setItem("videoplayer-current-time", data.seconds)
+);
 
 
 
