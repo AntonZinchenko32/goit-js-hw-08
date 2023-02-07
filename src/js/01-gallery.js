325efa4385ab6ebbe2fadd5722fe9cd6f5421aa3
AntonZinchenko32 <zinchenko309@gmail.com>
2023-02-07 12:07:34 +0200

    Update 01-gallery.js

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -2,4 +2,37 @@
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
+const myGallery = document.querySelector(".gallery");
+
+createMarkup();
+
+new SimpleLightbox('.gallery a',{
+    captionDelay: "250"
+});
+
+function createMarkup() {
+    const items = [];
+
+    galleryItems.forEach(item => {
+        const { preview, original, description } = item;
+
+        const link = document.createElement("a");
+        link.classList.add("gallery__item");
+        link.setAttribute("href", original);
+
+        const image = document.createElement('img');
+        image.classList.add("gallery__image");
+
+        image.src = preview;
+        image.alt = description;
+        image.setAttribute("title", description);
+
+        link.append(image);
+
+        items.push(link);
+    });
+
+    myGallery.append(...items);
+}
+
 console.log(galleryItems);
