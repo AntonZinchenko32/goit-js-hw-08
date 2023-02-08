913d09c63b65d4e546d8cc789bd5d059e80a7e59
AntonZinchenko32 <zinchenko309@gmail.com>
2023-02-08 12:39:27 +0200

    Final Update feedback.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,10 +1,10 @@
-// var throttle = require('lodash.throttle');
+var throttle = require('lodash.throttle');
 
 const myForm = document.querySelector(".feedback-form");
+const feedback = { email: "", message: "" };
 
-myForm.addEventListener("input", handleInput);
+myForm.addEventListener("input", throttle(handleInput, 500));
 myForm.addEventListener("submit", handleSubmit);
-
 
 if (localStorage.getItem("feedback-form-state")) {
     const { email, message } = JSON.parse(localStorage.getItem("feedback-form-state"));
@@ -13,22 +13,22 @@ if (localStorage.getItem("feedback-form-state")) {
 }
 
 function handleInput(event) {
+
+    const trimmedValue = event.target.value.trim();
     
-localStorage.setItem("feedback-form-state", JSON.stringify(generateFeedback(event)));
+    event.target.name === "email"
+        ? feedback.email = trimmedValue
+        : feedback.message = trimmedValue
+    
+localStorage.setItem("feedback-form-state", JSON.stringify(feedback));
 }
 
 function handleSubmit(event) {
     event.preventDefault();
     
-    console.log(generateFeedback(event));
+    console.log(feedback);
     event.currentTarget.reset();
     localStorage.removeItem("feedback-form-state");
 }
 
-function generateFeedback(event) {
-    const feedback = { email: "", message: "" };
-    
-    feedback.event.target.name = event.target.value;
 
-    return feedback;
-}
